71856dd4cb598b0001de0b035d3b074afb8fa0e4
rnogara <r.nogara.dev@gmail.com>
2023-01-08 15:20:41 -0300

    fourth-project

---
+++ fourth-project-reactjs/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Layout from "./components/layout";
+import Profile from "./components/profile";
+import Repositories from "./components/repositories";
+import useGithub from "./hooks/githubHooks";
+import NoSearch from "./components/no-search";
+
+function App() {
+  const { githubState } = useGithub();
+  return (
+    <Layout>
+      {githubState.hasUser ? (
+        <>
+          {githubState.loading ? (
+            <p>Loading</p>
+          ) : (
+            <>
+              <Profile />
+              <Repositories />
+            </>
+          )}
+        </>
+      ) : (
+        <NoSearch />
+      )}
+    </Layout>
+  );
+}
+
+export default App;
